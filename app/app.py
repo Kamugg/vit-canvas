93e4afa379e2eeab211ae93382a2d6dfcc07e8ea
kamugg <eugenio.deluca1997@gmail.com>
2025-12-23 23:44:50 +0100

    Implemented app.py Missing only the ability to select which ViT model to load (ideally one that i fast but a bit imprecise and one that is heavier but more accurate)

--- app/app.py
+++ app/app.py
@@ -2,43 +2,78 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import cv2
 import numpy as np
-import onnx
 import onnxruntime
 
 @st.cache_resource
-def get_onnx_session():
-    onnx_model = onnx.load('experiments/chkps/microvit_run_0.onnx')
-    onnx.checker.check_model(onnx_model)
-    ort_session = onnxruntime.InferenceSession('experiments/chkps/microvit_run_35.onnx',
+def get_onnx_session() -> onnxruntime.InferenceSession:
+    """
+    Loads ONNX ViT.
+    :return: ONNX session
+    """
+    ort_session = onnxruntime.InferenceSession('experiments/chkps/microvit_run_29.onnx',
                                                providers=['CPUExecutionProvider'])
     return ort_session
 
 def preprocess_image(x: np.ndarray) -> np.ndarray:
+    """
+    Preprocesses the drawn digit to feed it to the ViT.
+    :param x: The drawn digit.
+    :return: Preprocessed drawn digit.
+    """
+
     x = (x / 255.0).astype(np.float32)
     x = (x - 0.1307) / 0.3081
-    x = x[None, None, ...]
+    x = x[np.newaxis, np.newaxis, ...]
     return x
 
-def forward_pass(session: onnxruntime.InferenceSession, prep_img: np.ndarray) -> np.ndarray:
+def forward_pass(session: onnxruntime.InferenceSession, prep_img: np.ndarray) -> dict:
+    """
+    Performs a forward pass of the ViT model and returns both the non-softmaxed scores and the attention matrices.
+    :param session: ONNX session.
+    :param prep_img: The preprocessed drawn digit.
+    :return: Dictionary of non-softmaxed scores and attention matrices.
+    """
+
     ort_inputs = {session.get_inputs()[0].name: prep_img}
     ort_outs = session.run(None, ort_inputs)
     output_names = [output.name for output in session.get_outputs()]
     results_dict = dict(zip(output_names, ort_outs))
-    scores = results_dict['scores']
-    e_x = np.exp(scores)
-    scores = e_x / np.sum(e_x, axis=-1, keepdims=True)
-    return scores
+    return results_dict
 
+def overlay_attention(digit: np.ndarray, att: np.ndarray) -> np.ndarray:
+    """
+    Overlays the attention matrix with the drawn digit.
+    :param digit: The drawn (pixelated) digit.
+    :param att: The attention matrix.
+    :return: The overlayed drawn digit.
+    """
+
+    # Convertion to BGR
+    digit_bgr = cv2.cvtColor(digit, cv2.COLOR_GRAY2BGR)
+    att = att[..., np.newaxis]
+
+    # Green layer
+    green_layer = np.zeros((CANVAS_BASE * CANVAS_FACTOR, CANVAS_BASE * CANVAS_FACTOR, 3),
+                           dtype=np.float32)
+    green_layer[:] = (0, 255, 0)
+    digit_float = digit_bgr.astype(np.float32)
+
+    # Alpha blending
+    blended = (green_layer * att) + (digit_float * (1.0 - att))
+    return blended.astype(np.uint8)
+
+# Constants
 CANVAS_FACTOR = 12
 CANVAS_BASE = 28
 STROKE_WIDTH = 15
 
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>Digit classifier with ViT</h1>", unsafe_allow_html=True)
-st.markdown("<br>"*4, unsafe_allow_html=True)
+st.space('medium')
 
 col1, col2, col3 = st.columns([1, 1, 1], vertical_alignment='top')
 
+# Canvas column
 with col1:
     st.write("### Draw a digit:")
     canvas_result = st_canvas(
@@ -54,12 +89,19 @@ with col1:
         key="canvas",
     )
 
+# Showcase to the user the resized input image for the ViT
 with col2:
     st.write("### What the ViT sees:")
     image = canvas_result.image_data
     if image is not None:
+
+        # Convert to grayscale
         gray_image = cv2.cvtColor(image, cv2.COLOR_RGBA2GRAY)
+
+        # "Pixelate". This is the vit raw input image
         mnist_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_AREA)
+
+        # Resize to show to the user
         display_image_pixelated = cv2.resize(
             mnist_image,
             (CANVAS_FACTOR * CANVAS_BASE, CANVAS_FACTOR * CANVAS_BASE),
@@ -67,13 +109,18 @@ with col2:
         )
         st.image(display_image_pixelated)
 
+# Forward pass is skipped if the canvas is empty
 if (canvas_result.image_data is not None) and (np.sum(canvas_result.image_data[:, :, :3]) > 0):
     img = preprocess_image(mnist_image)
     sess = get_onnx_session()
-    smaxed_scores = forward_pass(sess, img)
+    out = forward_pass(sess, img)
+    scores = out['scores']
+    expsum = np.sum(np.exp(scores))
+    smaxed_scores = np.exp(scores) / expsum
 else:
     smaxed_scores = np.zeros((1, 10))
 
+# Column with the softmaxed scores
 with col3:
     st.write("Probabilities:")
     with st.container(border=True):
@@ -85,3 +132,20 @@ with col3:
 
             with row_col2:
                 st.progress(float(s))
+
+# Showcase attention matrices, this pass is skipped if the canvas is empty
+if (canvas_result.image_data is not None) and (np.sum(canvas_result.image_data[:, :, :3]) > 0):
+    num_layers = len(out)-1
+    num_heads = out['cls_att_tblock_0'].shape[1]
+    for i in range(num_layers):
+        st.write(f'### Transformer block {i+1}')
+        with st.container(border=True):
+            cols = st.columns(num_heads)
+            for c, col in enumerate(cols):
+                with col:
+                    att_map = out[f'cls_att_tblock_{i}'][0, c]
+                    resized_att_map = cv2.resize(att_map,
+                                           (CANVAS_FACTOR * CANVAS_BASE, CANVAS_FACTOR * CANVAS_BASE),
+                                                 interpolation=cv2.INTER_NEAREST)
+                    st.image(overlay_attention(display_image_pixelated, resized_att_map), caption=f'Head {c}')
+        st.space('small')
